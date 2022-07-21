1189d7de9ad613ae2d752be0a53e371492ad22af
Sayh0 <michaelyoon0948@gmail.com>
2022-07-21 12:43:38 +0900

    hello world

---
+++ Hello/src/test/main/MyClass.java
@@ -0,0 +1,11 @@
+package test.main; //이 클래스는 어느 package에 속해있는가에 대한 명시.
+
+public class MyClass { // Myclass 라는 이름의 클래스 정의하기.
+	public static void main(String[] args) { 
+		// 자바스크립트에서 function main(args){} 정도?
+		// 아 그리고 자바에서는 함수는 이제부터 method다 아쎼이!
+		// 메인메소드가 자바에서 일빠다. Run 누르면 이놈부터 실행됨. 유일무이
+		System.out.println("Hello, World!"); //헬로우 월드 프린트하기.
+	}
+	//자바를 배우는 현재의 목표 :DB 연동하기!
+}
